d92a91f6c4198e427fcab1df0f658187c4e62b7a
NATH AiT <blackait167@gmail.com>
2022-06-13 17:22:11 +0700

    Add project files.

---
+++ FPTShop/Models/FPT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FPTShop.Models
+{
+    public class FPT
+    {
+        public long BookID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
